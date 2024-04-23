fe7158b830c35f98144fac3f5bab84beb602adf0
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-04-23 10:40:13 +0900

    기본 프로젝트 세팅

---
+++ app/components/GlobalHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// styles
+import { HeaderContainer } from '../styles/GlobalHeaderStyles';
+
+const GlobalHeader = () => {
+  return (
+    <HeaderContainer>
+      <div>Talk to me◦</div>
+    </HeaderContainer>
+  );
+};
+
+export default GlobalHeader;
---
+++ app/styles/GlobalHeaderStyles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  box-shadow: 0 5px 5px 1px gray;
+
+`
