31101e4c472540bc7895057827424a218132bb54
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-04-23 18:01:34 +0900

    feat: 로그인 추가  퍼블리싱

--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -1,24 +1,31 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 // styles
 import { LoginContainer } from "@/app/styles/Login/LoginStyles";
 
 // img
 import ShowPwd from "@/app/assets/login/showPassword.png";
+import HidePwd from "@/app/assets/login/hidePassword.png";
 import Kakao from "@/app/assets/login/kakao_login_medium_wide.png";
 
 // libraries
 import axios from "axios";
 import Script from "next/script";
 
+// types
+import { LoginUserData } from "@/app/types/aboutLogin";
+
 const page = () => {
   const router = useRouter();
   // 비밀번호 보이기
   const [changePasswordMode, setChangePasswordMode] = useState(false);
-  const [kakaoLoginData, setKakaoLoginData] = useState([]);
+  const [loginData, setLoginData] = useState<LoginUserData>({
+    email: "",
+    password: "",
+  });
 
   // 비밀번호 상태 변경
   const handlePWD = () => {
@@ -30,18 +37,39 @@ const page = () => {
     router.push("/pages/signup");
   };
 
+  // 홈페이지 이동
+  const goHome = () => {
+    router.push("/");
+  };
+
+  // 로그인 데이터
+  const inputData = (sort: string, value: string) => {
+    setLoginData((prev) => ({
+      ...prev,
+      [sort]: value,
+    }));
+  };
+
+  // 추 후 backend 구축 후에 카카오 소셜 로그인 작업 예정
   const kakaoLogin = () => {};
 
   useEffect(() => {
-    console.log("kakaoLoginData: ", kakaoLoginData);
-  }, [kakaoLoginData]);
+    console.log("loginData: ", loginData);
+  }, [loginData]);
   return (
     <LoginContainer>
       <span className="projectTitle">Talk to me◦</span>
 
       <main className="inputContainer">
         <div className="emailInput">
-          <input className="input" type="email" placeholder="email" required />
+          <input
+            className="input"
+            type="email"
+            placeholder="email"
+            name="email"
+            required
+            onChange={(e) => inputData("email", e.target.value)}
+          />
         </div>
 
         <div className="pwdInput">
@@ -49,17 +77,27 @@ const page = () => {
             className="input"
             type={changePasswordMode ? "text" : "password"}
             placeholder="password"
+            name="password"
             required
+            onChange={(e) => inputData("password", e.target.value)}
           />
-          <Image src={ShowPwd} alt="비밀번호 보이기" onClick={handlePWD} />
+          <Image
+            src={changePasswordMode ? HidePwd : ShowPwd}
+            alt="비밀번호 보이기"
+            onClick={handlePWD}
+          />
         </div>
+
+        {/* 로그인 error 시에 */}
         <div className="errorMessage">
           {" "}
           이메일 혹은 비밀번호를 확인해주세요 ❌{" "}
         </div>
       </main>
 
-      <button className="loginButton">로그인</button>
+      <button className="loginButton" onClick={goHome}>
+        로그인
+      </button>
 
       <div style={{ color: "#b6b4b4", marginTop: "3rem" }}>
         아직 계정이 없으신가요??
